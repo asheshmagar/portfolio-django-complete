6c090978fbf4d528fd38c281ccc090d7d3c53116
asheshmagar <asheshmagar8888667@gmail.com>
2020-03-07 10:44:36 +0545

    django updated

---
+++ portfolioenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\ashes\Desktop\portfolio-django-complete\portfolioenv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
